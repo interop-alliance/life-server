889b55ee8f5b0b9e2b099d37f9c510134db994a5
Dmitri Zagidulin <dzagidulin@gmail.com>
2018-07-07 19:57:44 -0400

    Remove examples/ folder (obsolete)

--- examples/simple-ldp-server.js
+++
@@ -1,8 +0,0 @@
-var solid = require('../') // or require('solid-server')
-
-// Startin solid server
-var ldp = solid.createServer()
-ldp.listen(3456, function () {
-  console.log('Starting server on port ' + 3456)
-  console.log('LDP will run on /')
-})
