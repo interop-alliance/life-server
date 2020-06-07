f08af7b152dc1b72f43084a3f5414dd694b12e46
Dmitri Zagidulin <dzagidulin@gmail.com>
2020-06-07 12:58:43 -0400

    Add provisionAccountDid() test.

--- test/integration/account-manager-test.js
+++ test/integration/account-manager-test.js
@@ -18,7 +18,11 @@ const accountTemplatePath = path.join(__dirname, '..', '..',
 let host
 
 beforeEach(() => {
-  host = ServerHost.from({ serverUri: 'https://example.com' })
+  host = ServerHost.from({
+    serverUri: 'https://example.com',
+    root: testAccountsDir,
+    multiuser: true
+  })
   fs.removeSync(path.join(__dirname, '..', 'resources', 'accounts', 'alice.localhost'))
 })
 
@@ -37,13 +41,13 @@ describe('AccountManager', () => {
       const options = testAccountManagerOptions(host)
       const accountManager = AccountManager.from(options)
 
-      it('resolves to true if a directory for the account exists in root', async () => {
+      it('resolves to true if account directory exists', async () => {
         // Note: test/resources/accounts/tim.localhost/ exists in this repo
         const exists = await accountManager.accountExists('tim')
         expect(exists).to.be.true()
       })
 
-      it('resolves to false if a directory for the account does not exist', async () => {
+      it('resolves to false if account directory does not exist', async () => {
         // Note: test/resources/accounts/alice.localhost/ does NOT exist
         const exists = await accountManager.accountExists('alice')
         expect(exists).to.be.false()
@@ -64,31 +68,24 @@ describe('AccountManager', () => {
           })
       })
 
-      it('resolves to false if root .acl does not exist in root storage', () => {
-        host.root = testAccountsDir
+      it('resolves to false if root .acl does not exist', async () => {
         host.multiuser = false
 
         const options = testAccountManagerOptions(host)
         const accountManager = AccountManager.from(options)
 
-        return accountManager.accountExists()
-          .then(exists => {
-            expect(exists).to.be.false()
-          })
+        const exists = await accountManager.accountExists()
+        expect(exists).to.be.false()
       })
     })
   })
 
   describe('provisionAccountStorage()', () => {
     it('should create an account directory', async () => {
-      host.root = testAccountsDir
-      host.multiuser = true
-
       const options = testAccountManagerOptions(host)
       const accountManager = AccountManager.from({
         accountTemplatePath, ...options
       })
-
       const userData = {
         username: 'alice',
         email: 'alice@example.com',
@@ -96,9 +93,9 @@ describe('AccountManager', () => {
       }
       const userAccount = accountManager.userAccountFrom(userData)
 
-      const accountDir = path.join(testAccountsDir, 'alice.example.com')
-
       await accountManager.provisionAccountStorage(userAccount)
+
+      const accountDir = path.join(testAccountsDir, 'alice.example.com')
       const found = await accountManager.accountExists('alice')
       expect(found).to.be.true()
       const profile = fs.readFileSync(path.join(accountDir, 'web'), 'utf8')
@@ -107,6 +104,26 @@ describe('AccountManager', () => {
       const rootAcl = fs.readFileSync(path.join(accountDir, '.acl'), 'utf8')
       expect(rootAcl).to.include('<mailto:alice@')
       expect(rootAcl).to.include('<https://alice.example.com/web#id>')
+    })
+  })
+
+  describe('provisionAccountDid()', () => {
+    it('should create an account DID Document', async () => {
+      const options = testAccountManagerOptions(host)
+      const accountManager = AccountManager.from({
+        accountTemplatePath, ...options
+      })
+      const userData = { username: 'alice' }
+      const userAccount = accountManager.userAccountFrom(userData)
+
+      await accountManager.provisionAccountDid({ userAccount })
+
+      const didDocPath = path.join(testAccountsDir, 'alice.example.com',
+        '.well-known', 'did.json')
+
+      const didDoc = JSON.parse(await fs.readFile(didDocPath, 'utf8'))
+
+      expect(didDoc.id).to.equal('did:web:alice.example.com')
     })
   })
 })
