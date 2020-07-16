0e295672349e314a0d0d63835d4e9938c5496f83
Dmitri Zagidulin <dzagidulin@gmail.com>
2020-07-16 12:05:00 -0400

    Update Register form to use Materialize css.

---
+++ common/js/ready.js
@@ -0,0 +1,10 @@
+/**
+ * Util helper function for common use in internal pages.
+ */
+function onDocumentReady (fn) {
+  if(document.readyState !== 'loading') {
+    fn()
+  } else {
+    document.addEventListener('DOMContentLoaded', fn)
+  }
+}
