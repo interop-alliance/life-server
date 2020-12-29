3900473c6228b59edf8a8449da09fcb5ff51c6fb
Dmitri Zagidulin <dzagidulin@gmail.com>
2020-12-28 21:14:17 -0500

    Fix minor lint error.

--- common/js/ready.js
+++ common/js/ready.js
@@ -2,7 +2,7 @@
  * Util helper function for common use in internal pages.
  */
 function onDocumentReady (fn) {
-  if(document.readyState !== 'loading') {
+  if (document.readyState !== 'loading') {
     fn()
   } else {
     document.addEventListener('DOMContentLoaded', fn)
